8ec0258f29c5ced97ab0d851c6de88845db4fc51
Rafiul29 <rafiulislam4472@gmail.com>
2024-12-09 13:31:26 +0600

    update code

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const morgan=require('morgan')
-const cors = require('cors')
+const morgan = require("morgan");
+const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerDoc = YAML.load("./swagger.yaml");
 const OpenApiValidator = require("express-openapi-validator");
+const path = require("path");
+
+const swaggerDoc = YAML.load(path.resolve(__dirname, "./swagger.yaml"));
 
 const applyMiddleware = (app) => {
-
   app.use(express.json());
-  app.use(morgan('dev'))
-  app.use(cors())
+  app.use(morgan("dev"));
+  app.use(cors());
   app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
   app.use(
     OpenApiValidator.middleware({
-      apiSpec: "swagger.yaml",
+      apiSpec: path.resolve(__dirname, "./swagger.yaml"),
     })
   );
 };
